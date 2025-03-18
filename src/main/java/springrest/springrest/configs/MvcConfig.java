3d536c2e9a844c7e1c41a39e39e2eeedfa2ce9f2
Vegas2101 <matusov.210121@gmail.com>
2025-03-18 20:16:16 +0300

    😎👌🔥

--- src/main/java/springrest/springrest/configs/MvcConfig.java
+++ src/main/java/springrest/springrest/configs/MvcConfig.java
@@ -10,5 +10,6 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/user").setViewName("user");
+        registry.addViewController("/admin").setViewName("admin");
     }
 }
--- src/main/java/springrest/springrest/configs/SuccessUserHandler.java
+++ src/main/java/springrest/springrest/configs/SuccessUserHandler.java
@@ -18,9 +18,9 @@ public class SuccessUserHandler implements AuthenticationSuccessHandler {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
 
         if (roles.contains("ROLE_ADMIN")) {
-            response.sendRedirect("/admin.html");
+            response.sendRedirect("/admin");
         } else if (roles.contains("ROLE_USER")) {
-            response.sendRedirect("/user.html");
+            response.sendRedirect("/user");
         }
     }
 }
